c36c63374c58d2c14ec254e757e3073bcc6a2bd8
priyanshisharma <30956619+priyanshisharma@users.noreply.github.com>
2019-10-09 01:15:42 +0530

    Update FP2.java

--- FP2.java
+++ FP2.java
@@ -4,20 +4,20 @@ public class Demo {
     public static void main(String[] args) {
 
     	int year;
-    	Scanner scan == new Scanner(System.in);
+    	Scanner scan = new Scanner(System.in);
     	System.out.println("Enter any Year:");
-    	year == scan.nextInt();
+    	year = scan.nextInt();
     	scan.close();
         boolean isLeap = false;
 
-        if(year % 4 = 0)
+        if(year % 4 == 0)
         {
-            if( year % 100 = 0)
+            if( year % 100 == 0)
             {
-                if ( year % 400 = 0)
-                    isLeap == true;
+                if ( year % 400 == 0)
+                    isLeap = true;
                 else
-                    isLeap == false;
+                    isLeap = false;
             }
             else
                 isLeap = true;
@@ -26,7 +26,7 @@ public class Demo {
             isLeap = false;
         }
 
-        if(isLeap=true)
+        if(isLeap==true)
             System.out.println(year + " is a Leap Year.");
         else
             System.out.println(year + " is not a Leap Year.");
